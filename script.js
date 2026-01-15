871df6862ce4e463feb6a971a8e72d95fe536f9d
ajay-7078 <ajayk889087@gmail.com>
2026-01-15 14:41:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const heading = document.querySelector(".hero-text h1");
+
+window.addEventListener("load", (e) => {
+  heading.style.transition = "0.8s ease";
+  heading.style.opacity = 1;
+  heading.style.transform = "translateY(0)";
+});
